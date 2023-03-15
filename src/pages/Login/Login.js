5d709cdacab94578ed0b53e75177355e427ced41
Barnabás Várszegi <varszegibarnabas@gmail.com>
2023-03-15 03:05:25 +0000

    Update Login.js

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -16,7 +16,7 @@ const Login = ({ dispatch }) => {
       await api.createSession(email, password);
       const data = await api.getAccount();
       dispatch({ type: FetchState.FETCH_SUCCESS, payload: data });
-      console.log(data)
+      console.log(data);
     } catch (e) {
       dispatch({ type: FetchState.FETCH_FAILURE });
     }
@@ -30,14 +30,14 @@ const Login = ({ dispatch }) => {
         <h1 className="text-6xl font-bold">Belépés</h1>
         <p className="mt-6">
           {" "}
-          <strong>Nincs még VEG-es Microsoft Fiókod?</strong>
-          {" "}<br/>
+          <strong>Nincs még VEG-es Microsoft Fiókod? Csinalj!!!</strong> <br />
           <span
             className="cursor-pointer underline"
             onClick={() => setRegister(true)}
           >
             Regisztrálok
-          </span>{" "}ideiglenes felhasználó névvel.
+          </span>{" "}
+          ideiglenes felhasználó névvel.
         </p>
         <form onSubmit={handleLogin}>
           <label className="block mt-6"> Email</label>
@@ -57,12 +57,12 @@ const Login = ({ dispatch }) => {
             autoComplete="password"
           />
           <div>
-          <div
-            // type="submit"
-            className="w-full p-4 mx-auto mt-4 py-3 lg:py-5 px-10 lg:px-24 text-lg text-center md:text-2xl font-semibold cursor-pointer rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:bg-gray-100 focus:outline-none"
-          >
-            microsoft{windows(10, "#d84a4a")} belépés
-          </div>
+            <div
+              // type="submit"
+              className="w-full p-4 mx-auto mt-4 py-3 lg:py-5 px-10 lg:px-24 text-lg text-center md:text-2xl font-semibold cursor-pointer rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:bg-gray-100 focus:outline-none"
+            >
+              microsoft{windows(10, "#d84a4a")} belépés
+            </div>
           </div>
 
           <div className="mt-6">
